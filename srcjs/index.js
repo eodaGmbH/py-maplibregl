ad8bfe3b2a7ce18d7cb4326159d1320b7c27b776
Stefan Kuethe <stefan.kuethe@eoda.de>
2023-12-22 10:27:26 +0100

    Use prettier

--- srcjs/index.js
+++ srcjs/index.js
@@ -1,4 +1,4 @@
-console.log("Welcome to pymaplibregl!")
+console.log("Welcome to pymaplibregl!");
 
 /*
 if (Shiny) {
@@ -12,37 +12,44 @@ if (Shiny) {
 if (Shiny) {
   class MapLibreGLOutputBinding extends Shiny.OutputBinding {
     find(scope) {
-        console.log("I am here!");
-        return scope.find(".shiny-maplibregl-output");
+      console.log("I am here!");
+      return scope.find(".shiny-maplibregl-output");
     }
 
     renderValue(el, payload) {
-        console.log(el.id, payload);
-        const params = Object.assign({container: el.id}, payload.data.mapOptions)
-        this.map = new maplibregl.Map(params)
-        this.map.addControl(new maplibregl.NavigationControl());
+      console.log(el.id, payload);
+      const params = Object.assign(
+        { container: el.id },
+        payload.data.mapOptions,
+      );
+      this.map = new maplibregl.Map(params);
+      this.map.addControl(new maplibregl.NavigationControl());
 
-        // Add markers
-        this.map.on("load", () => payload.data.markers.forEach(({lngLat, popup, options}) => {
-            console.log(lngLat, popup, options);
-             const marker = new maplibregl.Marker(options).setLngLat(lngLat);
-             if (popup) {
-                 const popup_ = new maplibregl.Popup().setText(popup);
-                 marker.setPopup(popup_);
-             }
-             marker.addTo(this.map);
-        }));
+      // Add markers
+      this.map.on("load", () =>
+        payload.data.markers.forEach(({ lngLat, popup, options }) => {
+          console.log(lngLat, popup, options);
+          const marker = new maplibregl.Marker(options).setLngLat(lngLat);
+          if (popup) {
+            const popup_ = new maplibregl.Popup().setText(popup);
+            marker.setPopup(popup_);
+          }
+          marker.addTo(this.map);
+        }),
+      );
 
-        // Add layers
-        this.map.on("load", () => payload.data.layers.forEach((props) => {
-            console.log(props);
-            this.map.addLayer(props);
-        }));
+      // Add layers
+      this.map.on("load", () =>
+        payload.data.layers.forEach((props) => {
+          console.log(props);
+          this.map.addLayer(props);
+        }),
+      );
     }
   }
 
   Shiny.outputBindings.register(
-      new MapLibreGLOutputBinding(),
-      "shiny-maplibregl-output"
+    new MapLibreGLOutputBinding(),
+    "shiny-maplibregl-output",
   );
 }
