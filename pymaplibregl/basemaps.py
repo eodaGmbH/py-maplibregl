87e5a47fa546c38ca33a157d48ab7e5855acb7d4
Stefan Kuethe <stefan.kuethe@eoda.de>
2023-12-26 23:58:51 +0100

    Refactor

--- pymaplibregl/basemaps.py
+++ pymaplibregl/basemaps.py
@@ -13,11 +13,17 @@ def construct_carto_basemap_url(style_name: str = "dark-matter") -> str:
     return f"https://basemaps.cartocdn.com/gl/{Carto(style_name).value}-gl-style/style.json"
 
 
+def construct_basemap_style(
+    name: str = "nice-style", sources: dict = {}, layers: list = []
+) -> dict:
+    layers = [layer.data if isinstance(layer, Layer) else layer for layer in layers]
+    return {"name": name, "version": 8, "sources": sources, "layers": layers}
+
+
 def background(color: str = "black", opacity: float = 1.0) -> dict:
     bg_layer = Layer(
         LayerType.BACKGROUND,
         source=None,
         paint={"background-color": color, "background-opacity": opacity},
-    ).data
-    style = {"version": 8, "sources": {}, "layers": [bg_layer]}
-    return style
+    )
+    return construct_basemap_style(layers=[bg_layer])
