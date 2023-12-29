c10ac4d5dfb547b7f51f836c1ba6b4b7f52d537b
Stefan Kuethe <stefan.kuethe@eoda.de>
2023-12-29 17:55:39 +0100

    Add interaction to example

--- examples/every_person_in_manhattan/app.py
+++ examples/every_person_in_manhattan/app.py
@@ -1,11 +1,20 @@
 import pandas as pd
-from pymaplibregl import Layer, LayerType, Map, output_maplibregl, render_maplibregl
+from pymaplibregl import (
+    Layer,
+    LayerType,
+    Map,
+    MapContext,
+    output_maplibregl,
+    render_maplibregl,
+)
 from pymaplibregl.basemaps import Carto
 from pymaplibregl.utils import df_to_geojson
-from shiny import App, ui
+from shiny import App, reactive, ui
 
 MALE_COLOR = "rgb(0, 128, 255)"
 FEMALE_COLOR = "rgb(255, 0, 128)"
+LAYER_ID = "every-person-in-manhattan"
+CIRCLE_RADIUS = 2
 
 point_data = pd.read_json(
     "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/scatterplot/manhattan.json"
@@ -18,11 +27,11 @@ every_person_in_manhattan_source = {
 
 every_person_in_manhattan_layer = Layer(
     LayerType.CIRCLE,
-    id_="every-person-in-manhattan",
+    id_=LAYER_ID,
     source=every_person_in_manhattan_source,
     paint={
         "circle-color": ["match", ["get", "sex"], 1, MALE_COLOR, FEMALE_COLOR],
-        "circle-radius": 2,
+        "circle-radius": CIRCLE_RADIUS,
     },
 )
 
@@ -31,6 +40,7 @@ center = [-118.0931, 33.78615]
 app_ui = ui.page_fluid(
     ui.panel_title("Every Person in Manhattan"),
     output_maplibregl("maplibre", height=600),
+    ui.input_slider("radius", "Radius", value=CIRCLE_RADIUS, min=1, max=5),
 )
 
 
@@ -40,6 +50,12 @@ def server(input, output, session):
         m = Map(style=Carto.POSITRON, center=[-73.987157, 40.729906], zoom=12)
         m.add_layer(every_person_in_manhattan_layer)
         return m
+
+    @reactive.Effect
+    @reactive.event(input.radius, ignore_init=True)
+    async def radius():
+        async with MapContext("maplibre") as m:
+            m.set_paint_property(LAYER_ID, "circle-radius", input.radius())
 
 
 app = App(app_ui, server)
