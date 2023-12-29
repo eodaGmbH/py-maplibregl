8437749d711db527a01e993fbaeb13fe7bdeed63
Stefan Kuethe <stefan.kuethe@eoda.de>
2023-12-29 12:18:22 +0100

    Test pydantic source

--- pymaplibregl/source.py
+++ pymaplibregl/source.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 from enum import Enum
+from typing import Union
+
+from pydantic import BaseModel, ConfigDict, Field, computed_field
 
 
 class SourceType(Enum):
@@ -10,3 +13,21 @@ class SourceType(Enum):
     GEOJSON = "geojson"
     IMAGE = "image"
     VIDEO = "video"
+
+
+class SourceModel(BaseModel):
+    model_config = ConfigDict(validate_assignment=True, extra="allow")
+
+    def to_dict(self) -> dict:
+        return self.model_dump(by_alias=True, exclude_none=True)
+
+
+class GeoJSONSource(SourceModel):
+    data: Union[str, dict]
+    cluster_radius: int = Field(None, serialization_alias="clusterRadius")
+    cluster_min_points: int = Field(None, serialization_alias="clusterMinPoints")
+
+    @computed_field
+    @property
+    def type(self) -> str:
+        return SourceType.GEOJSON.value
---
+++ tests/test_sources.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from pymaplibregl.source import *
+
+
+def test_geojson_source():
+    # Prepare
+    data = "https://raw.githubusercontent.com"
+
+    # Act
+    geojson_source = GeoJSONSource(
+        data=data, test="test", cluster_radius=2, cluster_min_points=10
+    )
+
+    print(geojson_source.model_dump(exclude_none=True, by_alias=True))
+    print(geojson_source.to_dict())
+    print(dict(geojson_source))
