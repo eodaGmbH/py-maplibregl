5b7dc96cbf9c1ab5c0a7e26eda157fdedecdef22
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-01-02 13:28:36 +0100

    Update earthquakes example

--- examples/earthquakes_cluster/app.py
+++ examples/earthquakes_cluster/app.py
@@ -1,4 +1,4 @@
-from pymaplibregl import Layer, Map, output_maplibregl, render_maplibregl
+from pymaplibregl import Layer, Map, MapOptions, output_maplibregl, render_maplibregl
 from pymaplibregl.basemaps import Carto
 from pymaplibregl.sources import GeoJSONSource
 from shiny import App, reactive, ui
@@ -7,15 +7,20 @@ SOURCE_ID = "earthquakes"
 LAYER_ID = "earthquakes"
 
 earthquakes_source = GeoJSONSource(
-    data="https://raw.githubusercontent.com/crazycapivara/mapboxer/master/data-raw/earthquakes.geojson",
+    # data="https://raw.githubusercontent.com/crazycapivara/mapboxer/master/data-raw/earthquakes.geojson",
+    data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson",
     cluster=True,
     cluster_radius=50,
     cluster_min_points=2,
     cluster_max_zoom=14,
+    cluster_properties={
+        "maxMag": ["max", ["get", "mag"]],
+        "minMag": ["min", ["get", "mag"]],
+    },
 )
 
 earthquakes_layer = Layer(
-    "circle",
+    type="circle",
     source=SOURCE_ID,
     paint={"circle-color": "darkblue"},
     filter=["!", ["has", "point_count"]],
@@ -23,27 +28,21 @@ earthquakes_layer = Layer(
 
 
 earthquakes_cluster_layer = Layer(
-    "circle",
-    id_=LAYER_ID,
+    type="circle",
+    id=LAYER_ID,
     source=SOURCE_ID,
     filter=["has", "point_count"],
     paint={
-        "circle-color": "darkred",
-        "circle-radius": [
+        "circle-color": [
             "step",
             ["get", "point_count"],
-            10,
-            50,
-            15,
+            "#51bbd6",
             100,
-            20,
-            200,
-            25,
-            300,
-            30,
-            400,
-            40,
+            "#f1f075",
+            750,
+            "#f28cb1",
         ],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
     },
 )
 
@@ -51,7 +50,7 @@ earthquakes_cluster_layer = Layer(
 center = [-118.0931, 33.78615]
 
 app_ui = ui.page_fluid(
-    ui.panel_title("Hello PyMapLibreGL!"),
+    ui.panel_title("Earthquakes Cluster"),
     output_maplibregl("maplibre", height=500),
 )
 
@@ -59,11 +58,23 @@ app_ui = ui.page_fluid(
 def server(input, output, session):
     @render_maplibregl
     async def maplibre():
-        m = Map(style=Carto.POSITRON, center=center, zoom=5)
+        m = Map(MapOptions(style=Carto.POSITRON, center=([-103.5917, 40.6699]), zoom=3))
         m.add_source(SOURCE_ID, earthquakes_source)
         m.add_layer(earthquakes_cluster_layer)
-        m.add_popup(LAYER_ID, "point_count")
+        m.add_popup(LAYER_ID, "maxMag")
         m.add_layer(earthquakes_layer)
+        m.add_layer(
+            Layer(
+                type="symbol",
+                id="text",
+                source=SOURCE_ID,
+                filter=["has", "point_count"],
+                layout={
+                    "text-field": ["get", "point_count_abbreviated"],
+                    "text-size": 12,
+                },
+            )
+        )
         return m
 
     @reactive.Effect
