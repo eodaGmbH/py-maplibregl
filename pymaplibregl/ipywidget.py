cd77ee3f69df54380a3010faa30917ace02a421e
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-01-04 18:26:54 +0100

    Refactor js

--- pymaplibregl/ipywidget.py
+++ pymaplibregl/ipywidget.py
@@ -13,6 +13,7 @@ class MaplibreWidget(AnyWidget):
     map_options = traitlets.Dict().tag(sync=True)
     height = traitlets.Union([traitlets.Int(), traitlets.Unicode()]).tag(sync=True)
     test = traitlets.Unicode().tag(sync=True)
+    calls = traitlets.List().tag(sync=True)
 
     def __init__(self, map_options=MapOptions(), **kwargs):
         self.map_options = map_options.to_dict()
--- pymaplibregl/srcjs/ipymaplibregl.js
+++ pymaplibregl/srcjs/ipymaplibregl.js
@@ -1,22 +1,33 @@
 import maplibregl from "https://esm.sh/maplibre-gl@3.6.2";
 
-export function render({ model, el }) {
-  console.log("maplibregl", maplibregl.version);
+function create_container(model) {
   const ID = "pymaplibregl";
   const container = document.createElement("div");
   container.id = ID;
   container.style.height = model.get("height");
-  const mapOptions = Object.assign(
-    { container: container },
-    model.get("map_options"),
-  );
-  console.log(mapOptions);
+  return container;
+}
+
+function create_map(mapOptions) {
   const map = new maplibregl.Map(mapOptions);
   map.addControl(new maplibregl.NavigationControl());
   map.once("load", () => {
     map.resize();
   });
+  return map;
+}
+
+export function render({ model, el }) {
+  console.log("maplibregl", maplibregl.version);
+
+  const container = create_container(model);
   el.appendChild(container);
+  const mapOptions = Object.assign(
+    { container: container },
+    model.get("map_options"),
+  );
+  console.log(mapOptions);
+  const map = create_map(mapOptions);
 
   model.on("change:test", () => {
     const test = model.get("test");
