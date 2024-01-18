54580dac92ed3401fccdd5c3bdbc704b7a369ccf
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-01-18 13:23:04 +0100

    Add custom call to example

--- docs/examples/getting_started/basic_usage_shiny.py
+++ docs/examples/getting_started/basic_usage_shiny.py
@@ -4,6 +4,7 @@ from shiny import App, reactive, ui
 
 app_ui = ui.page_fluid(
     output_maplibregl("maplibre", height=600),
+    ui.div("Click on map to set a marker"),
 )
 
 
@@ -18,7 +19,10 @@ def server(input, output, session):
     async def coords():
         async with MapContext("maplibre") as m:
             print(input.maplibre())
-            m.add_marker(Marker(lng_lat=input.maplibre()["coords"].values()))
+            lng_lat = tuple(input.maplibre()["coords"].values())
+            marker = Marker(lng_lat=lng_lat)
+            m.add_marker(marker)
+            m.add_call("flyTo", {"center": lng_lat})
 
 
 app = App(app_ui, server)
