8bcbda780f3aaafd059693dfb33b2e7577b4f5a3
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-01-19 13:19:07 +0100

    Add animation example

--- docs/examples/hike/app.py
+++ docs/examples/hike/app.py
@@ -18,14 +18,27 @@ SOURCE_ID = "hike"
 LAYER_ID = "hike"
 data = gpd.read_file("https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson")
 # print(data)
-print(data.get_coordinates().to_numpy()[0])
+# print(data.get_coordinates().to_numpy()[0])
+
+
+def create_feature(lng_lat) -> dict:
+    return {
+        "type": "Feature",
+        "geometry": {"type": "LineString", "coordinates": [lng_lat]},
+    }
+
+
+feature = create_feature(tuple(data.get_coordinates().to_numpy()[0]))
+feature_collection = {"type": "FeatureCollection", "features": [feature]}
+print(feature_collection)
 
 hike = GeoJSONSource(data="https://docs.mapbox.com/mapbox-gl-js/assets/hike.geojson")
 layer = Layer(
     type=LayerType.LINE,
     id=LAYER_ID,
     source="hike",
-    paint={"line-color": "yellow", "line-width": 3},
+    paint={"line-color": "orange", "line-width": 3},
+    layout={"visibility": "visible"},
 )
 
 app_ui = ui.page_fluid(
@@ -42,6 +55,15 @@ def server(input, output, session):
         )
         m.add_source(SOURCE_ID, hike)
         m.add_layer(layer)
+        m.add_source("hike-animation", GeoJSONSource(data=feature_collection))
+        m.add_layer(
+            Layer(
+                type=LayerType.LINE,
+                id="hike-animation",
+                source="hike-animation",
+                paint={"line-color": "yellow", "line-width": 5},
+            )
+        )
         # m.add_marker(Marker(lng_lat=data.get_coordinates().to_numpy()[0]))
         return m
 
@@ -50,10 +72,16 @@ def server(input, output, session):
     async def run():
         print("run")
         for lng_lat in data.get_coordinates().to_numpy():
+            print(lng_lat)
+            feature_collection["features"][0]["geometry"]["coordinates"].append(
+                tuple(lng_lat)
+            )
             async with MapContext("mapylibre") as m:
-                m.add_marker(Marker(lng_lat=lng_lat))
-                m.add_call("setCenter", tuple(lng_lat))
-            # await asyncio.sleep(2)
+                m.set_data("hike-animation", feature_collection)
+                m.add_call("panTo", tuple(lng_lat))
+            await asyncio.sleep(0.005)
+
+        feature_collection["features"][0]["geometry"]["coordinates"] = []
 
 
 app = App(app_ui, server)
