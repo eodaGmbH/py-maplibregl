6481815a70cb34e3257a2b0c48ee16a3f210ae76
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-01-21 12:29:18 +0100

    Update example

--- docs/examples/where_is_the_iss/app.py
+++ docs/examples/where_is_the_iss/app.py
@@ -12,6 +12,8 @@ from maplibre.sources import GeoJSONSource
 from shiny import App, reactive, ui
 
 MAX_FEATURES = 30
+SOURCE_ID_ISS_POSITION = "iss_position"
+SOURCE_ID_ISS_LAST_POSITIONS = "iss-last-positions"
 
 
 def where_is_the_iss() -> tuple:
@@ -19,24 +21,27 @@ def where_is_the_iss() -> tuple:
     return (r["longitude"], r["latitude"])
 
 
-def create_feature(lng_lat) -> dict:
+def create_feature(lng_lat: tuple) -> dict:
     return {
         "type": "Feature",
-        "geometry": {"type": "Point", "coordinates": lng_lat},
+        "geometry": {
+            "type": "Point",
+            "coordinates": lng_lat,
+        },
+        "properties": {"coords": ", ".join(map(str, lng_lat))},
     }
 
 
 lng_lat = where_is_the_iss()
-# print(lng_lat)
-
-feature = create_feature(lng_lat)
-# print(feature)
-
+feature = create_feature(where_is_the_iss())
 feature_collection = {"type": "FeatureCollection", "features": [feature]}
-# print(feature_collection)
 
 app_ui = ui.page_fluid(
-    ui.h1("Where is the ISS"),
+    ui.panel_title("Where is the ISS"),
+    ui.div("Click on Update to get the current position of the ISS."),
+    ui.div(
+        "The yellow dots show the positions before. Hover the blue dot to display the coordinates."
+    ),
     output_maplibregl("mapylibre", height=600),
     ui.input_action_button("update", "Update"),
 )
@@ -46,14 +51,27 @@ def server(input, output, session):
     @render_maplibregl
     def mapylibre():
         m = Map(MapOptions(center=lng_lat, zoom=3))
-        m.add_source("iss", GeoJSONSource(data=feature_collection))
+        m.set_paint_property("water", "fill-color", "darkblue")
+        m.add_source(
+            SOURCE_ID_ISS_LAST_POSITIONS, GeoJSONSource(data=feature_collection)
+        )
         m.add_layer(
             Layer(
                 type=LayerType.CIRCLE,
-                source="iss",
+                source=SOURCE_ID_ISS_LAST_POSITIONS,
                 paint={"circle-color": "yellow", "circle-radius": 5},
             ),
         )
+        m.add_source(SOURCE_ID_ISS_POSITION, GeoJSONSource(data=feature))
+        m.add_layer(
+            Layer(
+                type=LayerType.CIRCLE,
+                id="iss-position",
+                source=SOURCE_ID_ISS_POSITION,
+                paint={"circle-color": "lightblue", "circle-radius": 7},
+            )
+        )
+        m.add_tooltip("iss-position", "coords")
         return m
 
     @reactive.Effect
@@ -66,9 +84,10 @@ def server(input, output, session):
             feature_collection["features"] = []
 
         async with MapContext("mapylibre") as m:
-            feature_collection["features"].append(create_feature(lng_lat))
-            print(feature_collection)
-            m.set_data("iss", feature_collection)
+            feature = create_feature(lng_lat)
+            m.set_data(SOURCE_ID_ISS_POSITION, feature)
+            feature_collection["features"].append(feature)
+            m.set_data(SOURCE_ID_ISS_LAST_POSITIONS, feature_collection)
             m.add_call("flyTo", {"center": lng_lat, "speed": 0.5})
 
 
