893bdfbbd3baae96b84ad4fdf1100f222b8669e5
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-05-07 17:22:28 +0200

    Use new renderer class

--- maplibre/__init__.py
+++ maplibre/__init__.py
@@ -4,6 +4,8 @@ from .controls import ControlPosition, ControlType
 from .layer import Layer, LayerType
 from .map import Map, MapOptions
 from .mapcontext import MapContext
-from .server import render_maplibregl
+
+# from .server import render_maplibregl
 from .shiny import MapLibreRenderer
+from .shiny import MapLibreRenderer as render_maplibregl
 from .ui import output_maplibregl
