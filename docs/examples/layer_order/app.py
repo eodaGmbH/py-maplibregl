35666047e54eae562113c5215b3dcce738fa9b48
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-05-24 19:08:11 +0200

    Add example

--- docs/examples/layer_order/app.py
+++ docs/examples/layer_order/app.py
@@ -1,3 +1,5 @@
+# Shiny Express App
+
 import requests as req
 from maplibre import Layer, LayerType, Map, MapOptions, render_maplibregl
 from maplibre.basemaps import Carto, construct_carto_basemap_url
@@ -15,21 +17,32 @@ urban_areas = GeoJSONSource(
 )
 
 m = Map(
-    MapOptions(style=style, center=(-88.13734351262877, 35.137451890638886), zoom=4)
+    MapOptions(
+        style=style,
+        center=(-89.928, 35.204),
+        # center=(-88.13734351262877, 35.137451890638886),
+        zoom=9,
+        hash=True,
+    )
 )
 m.add_layer(
     Layer(
         id="urban-areas-fill",
         type=LayerType.FILL,
         source=urban_areas,
-        paint={"fill-color": "#f08", "fill-opacity": 1.0},
+        paint={"fill-color": "pink", "fill-opacity": 1.0},
     ),
     before_id=symbol_ids[0],
 )
 for symbol_id in symbol_ids:
-    m.set_paint_property(symbol_id, "text-color", "darkblue")
+    m.set_paint_property(symbol_id, "text-color", "purple")
 
 
 @render_maplibregl
 def render_map():
     return m
+
+
+if __name__ == "__main__":
+    with open("docs/examples/layer_order/app.html", "w") as f:
+        f.write(m.to_html())
---
+++ docs/examples/layer_order/index.md
@@ -0,0 +1,13 @@
+Add a data layer below the labels of the basemap.
+
+<iframe src="app.html" height="620px", width="100%" style="border:none;"></iframe>
+
+```python
+-8<-- "layer_order/app.py"
+```
+
+Run example:
+
+```bash
+poetry run shiny run docs/examples/layer_order/app.py
+```
