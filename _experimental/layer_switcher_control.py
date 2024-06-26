d6bcc3fdd90f1337ec98eeae5d7ddcebe9aca6b3
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-06-26 16:12:19 +0200

    Add custom controls

---
+++ _experimental/layer_switcher_control.py
@@ -0,0 +1,55 @@
+# Shiny Express App
+
+import json
+import webbrowser
+
+from maplibre import Map, MapOptions, render_maplibregl
+from maplibre.basemaps import Carto
+from maplibre.controls import ControlPosition, NavigationControl, ScaleControl
+from maplibre.plugins import MapboxDrawControls, MapboxDrawOptions
+from maplibre.ui import use_mapboxgl_draw
+
+# from shiny import reactive
+from shiny.express import input, render, ui
+
+m = Map(
+    MapOptions(
+        style=Carto.POSITRON,
+        center=(-122.4, 37.74),
+        zoom=12,
+        hash=True,
+        pitch=40,
+    )
+)
+m.add_control(NavigationControl())
+m.add_control(ScaleControl(), ControlPosition.BOTTOM_LEFT)
+
+m.add_call(
+    "addControl",
+    "InfoBoxControl",
+    {
+        "cssText": "padding: 20px; font-size: 20px;font-family: monospace;",
+        "content": "<h1>Awesome control.</h1><p>And some text.</p>",
+    },
+    ControlPosition.TOP_LEFT.value,
+)
+
+
+@render_maplibregl
+def maplibre():
+    return m
+
+
+@render.code
+def selected_features():
+    obj = input.maplibre_draw_features_selected()
+    print(obj)
+    return json.dumps(obj["features"], indent=2) if obj else "Pick some features!"
+
+
+if __name__ == "__main__":
+    filename = "docs/examples/mapbox_draw_plugin/app.html"
+    with open(filename, "w") as f:
+        f.write(m.to_html())
+
+    webbrowser.open(filename)
---
+++ srcjs/custom-controls/info-box.js
@@ -0,0 +1,20 @@
+export default class InfoBoxControl {
+  constructor(options) {
+    this._options = options || {};
+  }
+
+  onAdd(map) {
+    this._map = map;
+    this._container = document.createElement("div");
+    // this._container.classList.add("maplibregl-ctrl", "maplibregl-ctrl-group");
+    this._container.className = "maplibregl-ctrl maplibregl-ctrl-group";
+    this._container.style.cssText = this._options.cssText || "";
+    this._container.innerHTML = this._options.content || "We out here.";
+    return this._container;
+  }
+
+  onRemove() {
+    this._container.parentNode.removeChild(this._container);
+    this._map = undefined;
+  }
+}
