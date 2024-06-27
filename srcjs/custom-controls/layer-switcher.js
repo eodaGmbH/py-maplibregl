bd5aa5e2a553c6d1fc2190ffb4f4e470a00cc361
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-06-27 19:18:02 +0200

    Refactor layer switcher control

--- srcjs/custom-controls/layer-switcher.js
+++ srcjs/custom-controls/layer-switcher.js
@@ -1,45 +1,32 @@
-import "../css/custom-layer-switcher-control.css"
+import "../css/custom-layer-switcher-control.css";
 
-function createSelect(layers) {
-  const select = document.createElement("select");
-  select.id = "layer-switcher";
-  select.style.padding = "5px";
-  select.multiple = true;
-  // select.size = 1;
-  for (let layer of layers) {
-    const option = document.createElement("option");
-    option.innerHTML = layer;
-    select.appendChild(option);
-  }
-
-  return select;
-}
+const THEMES = {
+  default: "layer-switcher-ctrl",
+};
 
 function createMenu(layerIds, map) {
   const menu = document.createElement("div");
   menu.id = "layer-switcher-menu";
-  for (const id of layerIds) {
+  for (const layerId of layerIds) {
     const link = document.createElement("a");
-    link.id = id;
+    link.id = layerId;
     link.href = "#";
-    link.textContent = id;
-    /*
-    link.style.cssText =
-      "display: block; text-align: center; background-color: white; padding: 5px; border-bottom: 1px solid rgba(0, 0, 0, 0.25); text-decoration: none";
-     */
-    link.className = 'active';
+    link.textContent = layerId;
+    if (map.getLayoutProperty(layerId, "visibility") === "visible") {
+      link.className = "active";
+    }
+
     link.onclick = function (e) {
-      const idClicked = this.textContent;
-      console.log(idClicked);
-      const visibility = map.getLayoutProperty(idClicked, "visibility");
-      console.log(visibility);
+      const layerIdClicked = this.textContent;
+      const visibility = map.getLayoutProperty(layerIdClicked, "visibility");
+      console.log(layerIdClicked, visibility);
       if (visibility === "visible") {
-        map.setLayoutProperty(idClicked, "visibility", "none");
+        map.setLayoutProperty(layerIdClicked, "visibility", "none");
         this.className = "";
         return;
       }
 
-      map.setLayoutProperty(idClicked, "visibility", "visible");
+      map.setLayoutProperty(layerIdClicked, "visibility", "visible");
       this.className = "active";
     };
     menu.appendChild(link);
@@ -49,34 +36,16 @@ function createMenu(layerIds, map) {
 
 export default class LayerSwitcherControl {
   constructor(options) {
-    this._options = options || {};
+    this._options = options;
   }
 
   onAdd(map) {
     this._map = map;
     this._container = document.createElement("div");
-    // this._container.classList.add("maplibregl-ctrl", "maplibregl-ctrl-group");
-    // this._container.className = "maplibregl-ctrl maplibregl-ctrl-group layer-switcher-ctrl";
-    this._container.className = "maplibregl-ctrl layer-switcher-ctrl";
-    this._container.style.cssText = this._options.cssText || "padding: 5px;";
-    // this._container.innerHTML = "We out here.";
-
-    // const layers = ["lay1", "layer2", "lay3", "awesome", "nice", "happy"];
-    const layerIds = this._options.layerIds; // ["landcover", "water", "landuse", "boundary_state"];
-    /*
-    for (let i in layers) {
-      const input = document.createElement("input");
-      input.type = "checkbox";
-      input.id = input.value = layers[i];
-      const label = document.createElement("label");
-      label.for = label.innerText = layers[i];
-      this._container.appendChild(input);
-      this._container.appendChild(label);
-    }
-    */
-    // this._container.appendChild(createSelect(layers));
+    this._container.classList.add("maplibregl-ctrl");
+    this._container.classList.add(THEMES[this._options.theme || "default"]);
+    const layerIds = this._options.layerIds;
     this._container.appendChild(createMenu(layerIds, map));
-
     return this._container;
   }
 
