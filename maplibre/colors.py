1ec41ad70cd86d5452047101914ca71e89e3acd1
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-07-02 14:15:02 +0200

    Refactor

--- maplibre/colors.py
+++ maplibre/colors.py
@@ -10,28 +10,28 @@ def rgb_to_hex(rgb: tuple) -> str:
 
 
 def color_palette_rgb(
-    rgb_source: tuple,
-    rgb_target: tuple,
+    source_rgb: tuple,
+    target_rgb: tuple,
     n: int,
     max_value: int = 255,
 ) -> list[tuple]:
     n = n - 1
-    steps = tuple(map(lambda i, j: (i - j) / n, rgb_target, rgb_source))
-    r, g, b = rgb_source
+    steps = tuple(map(lambda i, j: (i - j) / n, target_rgb, source_rgb))
+    r, g, b = source_rgb
     step_r, step_g, step_b = steps
     result = [
         (r + (i * step_r), g + (i * step_g), b + (i * step_b)) for i in range(n)
-    ] + [rgb_target]
+    ] + [target_rgb]
     if max_value == 255:
         return [tuple([round(item) for item in rgb]) for rgb in result]
 
     return result
 
 
-def color_palette(color_source: str, color_target: str, n: int) -> list[str]:
+def color_palette(source_color: str, target_color: str, n: int) -> list[str]:
     return [
         rgb_to_hex(color)
         for color in color_palette_rgb(
-            color_to_rgb(color_source), color_to_rgb(color_target), n
+            color_to_rgb(source_color), color_to_rgb(target_color), n
         )
     ]
