1e0a299777cf59fc7b9d1e8b98a83f433921eb18
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-07-13 00:07:14 +0200

    Add vancouver blocks express example

---
+++ _experimental/maplibre_express_vancouver_blocks.py
@@ -0,0 +1,19 @@
+from maplibre import express as mx
+
+path = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
+blocks = mx.read_file(path)
+breaks = blocks.valuePerSqm.quantile([0.25, 0.5, 0.75, 0.9]).to_list()
+
+filename = (
+    mx.CoreLayer(
+        blocks,
+        layer_type="fill",
+        color_column="valuePerSqm",
+        bins=breaks,
+        cmap="YlOrRd",
+    )
+    .to_map(style=mx.basemaps.Carto.POSITRON, pitch=35)
+    .save("/tmp/py-maplibre-express.html", preview=True)
+)
+
+print(filename)
